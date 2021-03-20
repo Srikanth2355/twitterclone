1434b261eb90ee4e737d508367de78f33ccc2c9c
Srikanth <gubbasrikanth2355@gmail.com>
2021-03-20 08:14:24 +0530

    getting data from twitter when user types in search box

--- js/index.js
+++ js/index.js
@@ -1,11 +1,28 @@
 const URL = "http://localhost:3000/tweets";
 
+const onenter = (e)=>{
+    if(e.key === "Enter"){
+        getTwitterData()
+    }
+}
 /**
  * Retrive Twitter Data from API
  */
 const getTwitterData = () => {
-
+    const query = document.getElementById("user-search-input").value;
+    if(!query) return;
+    const encodedquery = encodeURIComponent(query);
+    
+    const fullurl = `${URL}?q=${encodedquery}&count=10`;
+    fetch(fullurl).then((response)=> {
+        return response.json();
+    })
+    .then((data)=> {
+        console.log(data);
+    })
+    
 }
+
 
 /**
  * Save the next page data
